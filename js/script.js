9aecd6a2bd9be061a4d5ca0aa131115751ca22d2
Goran Crnković <goran.crnkovic@gmail.com>
2013-07-15 12:39:03 +0200

    Version 0.1a

---
+++ js/script.js
@@ -0,0 +1,20 @@
+(function($){
+  /**********************************
+  * Scrollspy                       * 
+  **********************************/
+  $('#navbar').scrollspy();
+  
+  /**********************************
+  * Smooth scroll                   * 
+  **********************************/
+  $('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+	    var target = this.hash,
+	    $target = $(target);
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 500, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+})(window.jQuery);
